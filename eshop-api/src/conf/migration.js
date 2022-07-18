21b5c4a008b9213388a97c1f6bef81d70017db5d
Restu Raina Rahmah <resturayinarahmah442@gmail.com>
2022-07-18 21:47:53 +0700

    error handling dengan custom exceptions

--- eshop-api/src/conf/migration.js
+++ eshop-api/src/conf/migration.js
@@ -1,6 +1,6 @@
 const migration = require('mysql-migrations');
 const Database = require('./Database');
 
-const database = new database();
+const database = new Database();
 
 migration.init(database.connection, __dirname + '/migrations');
--- eshop-api/src/server.js
+++ eshop-api/src/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 
+//authentication
+const authentication = require('./api/authentication');
+const Database = require('./conf/Database');
+const AuthenticationService = require('./services/mysql/AuthenticationService');
+const AuthenticationValidator = require('./validator/authentication');
+
 const init = async () => {
+  const database = new Database();
+  const authenticationServive = new AuthenticationService(database);
 
     const server = Hapi.server({
       host: process.env.HOST,
@@ -20,6 +28,35 @@ const init = async () => {
           name: 'Restu Raina Rahmah',
         }),
       });
+
+      //defines internal plugins
+      await server.register([
+        {
+          plugin: authentication,
+          option:{
+            service: AuthenticationService,
+            validation: AuthenticationValidator,
+          },
+        },
+      ]);
+
+    // extension
+  server.ext('onPreResponse', (request, h) => {
+    const {response} = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    console.log(response);
+
+    return h.continue;
+  });
     
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
